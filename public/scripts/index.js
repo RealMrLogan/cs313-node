8593ab77234af87f80f0fde05abce8a5c273e241
Logan Saunders <lsaunders275@gmail.com>
2019-07-13 17:20:26 -0600

    added a toggle list for the users

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,16 @@
 var socket = io.connect();
 
+// append the chat text message
+socket.on('chat_message', (msg) => {
+  $('#messages').append($('<li>').html(msg));
+});
+
+// append text if someone is online
+socket.on('is_online', (username) => {
+  $('#messages').append($('<li>').html(username));
+  getUsers();
+});
+
 function checkResponse(result, user) { // FIXME: this might be DRY
   switch (result) {
     case "success":
@@ -56,8 +67,20 @@ function startChatting(option, username) {
   }
   // hide the modal
   document.getElementById("login-modal").style.display = "none";
-  // show the message box
-  document.getElementById("send-message").style.display = "flex";
+  // show everything
+  document.getElementsByTagName("main")[0].style.display = "block";
+  // show the room name
+  displayRoomName(true);
+}
+
+function displayRoomName(isGeneral, roomName) {
+  if (isGeneral) {
+    document.getElementById("general-room-name").innerHTML = "General";
+    return;
+  }
+  // its a private room
+  console.log("It's a private room");
+  
 }
 
 function setAndAddUser(username) {
@@ -133,24 +156,28 @@ $('#send-message').submit((e) => {
   return false;
 });
 
-// append the chat text message
-socket.on('chat_message', (msg) => {
-  $('#messages').append($('<li>').html(msg));
-});
-
-// append text if someone is online
-socket.on('is_online', (username) => {
-  $('#messages').append($('<li>').html(username));
-});
-
 // call this function every 2.5 seconds to update the UI
-setInterval(getUsers, 2500);
+// setInterval(getUsers, 2500);
 function getUsers() {
   fetch('/getConnectedUsers').then(res => res.json()).then(response => {
-    console.log(response);
     if (response.length > 0) { // there is at least 1 user
+      // add to user list
+      addToUserList(response);
+      // show how many users
       updateUsers(response);
     }
+  });
+}
+
+function addToUserList(newList) {
+  const userList = document.getElementById("user-list");
+  // remove all the curent users
+  while (userList.firstChild) userList.removeChild(userList.firstChild);
+  // add the new user list
+  newList.forEach(user => {
+    const li = document.createElement("li");
+    li.innerHTML = user.name;
+    userList.appendChild(li);
   });
 }
 
@@ -176,7 +203,24 @@ function updateUsers(data) {
       break;
   }
   document.getElementById("num-users").innerHTML = data.length;
+
+  // toggle the user list
+  document.getElementById("user-icon").removeEventListener('click', toggleUserList)
+  document.getElementById("user-icon").addEventListener('click', toggleUserList)
 }
+
+function toggleUserList() {
+  const userList = document.getElementById("user-list");
+  const usersLoggedIn = document.getElementById("logged-in-users");
+  if (userList.style.display == "none") {
+    userList.style.display = "block";
+    usersLoggedIn.style.display = "none";
+  } else {
+    userList.style.display = "none";
+    usersLoggedIn.style.display = "block";
+  }
+}
+
 
 function isTyping() {
   console.log(document.cookie);
